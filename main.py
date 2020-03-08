8d147a57048bdbf4c032764117270dd06599c8f4
seb <touchetseb@gmail.com>
2020-03-08 09:01:02 +0100

    Correction du repertoire de destination dans le cas ou on lance plusieur fois

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ image_ext = ['.jpg', '.jpeg', 'cr2', 'cr3' 'nef', 'dng']
 #on recupere les photos du repertoire et des sous repertoire
 image_files = get_allfiles(Path_Photo)
 
+#Pour toutes les photos
 for image_file in image_files:
     image_pathfile = os.path.normpath(image_file)
     #MaPhoto = Photo(Path_Photo + "/" + image_file)
@@ -69,34 +70,41 @@ for image_file in image_files:
     PhotoExtention = MaPhoto.get_PhotoExtention()
     PhotoPath = MaPhoto.get_PhotoPath()
     print("Photo Source : " + MaPhoto.get_PhotoFullPathName())
+    #Si la photo n'a pas de data EXIF
     if MaPhoto.labeled == None:
         #print(MaPhoto.get_PhotoFullPathName() + " No Exif In this file")
         NouveauPathDePhoto = PhotoPath + "/" + "_A_TRIER_MANUELLEMENT"
         NouveauNomDePhoto = NouveauPathDePhoto + "/" + MaPhoto.get_PhotoName()
+    #Si la photo a des data EXIF
     else:
         #MaPhoto.print_PhotoExif()
+        #Si la photo n'a pas de date dans les data EXIF
         if MaPhoto.get_PhotoExifLabelValue("DateTimeOriginal") == None:
             print("Photo Move To >>>>>  NoExifInformationAvailable")
-            NouveauPathDePhoto = PhotoPath + "/" + "_A_TRIER_MANUELLEMENT"
+            NouveauPathDePhoto = Path_Photo + "/" + "_A_TRIER_MANUELLEMENT"
             NouveauNomDePhoto = NouveauPathDePhoto + "/" + MaPhoto.get_PhotoName()
 
         else:
+            #ON recupere le year et le month de la date de la photo avec le EXIF
             DateConverti = converter(MaPhoto.get_PhotoExifLabelValue("DateTimeOriginal"))
             PhotoYear = get_year(DateConverti)
             PhotoMonth = get_month(DateConverti)
 
+            #Si la photo a des data de geolocalisation dans EXIF
             GpsTags = MaPhoto.get_geotagging()
             if GpsTags != None:
                 GpsCoord = MaPhoto.get_coordinates(GpsTags)
                 MaPhoto.get_GpsElement(GpsCoord)
                 PhotoVille = MaPhoto.get_GpsElementVille()
                 PhotoRegion = MaPhoto.get_GpsElementRegion()
-                NouveauPathDePhoto = PhotoPath + "/" + PhotoYear + "/" + PhotoMonth + "/" + PhotoVille
+                NouveauPathDePhoto = Path_Photo + "/" + PhotoYear + "/" + PhotoMonth + "/" + PhotoVille
                 NouveauNomDePhoto = NouveauPathDePhoto + "/" + DateConverti + PhotoExtention
             else:
                 print("Pas de coordone GPS")
-                NouveauPathDePhoto = PhotoPath + "/" + "_A_TRIER_MANUELLEMENT"
-                NouveauNomDePhoto = NouveauPathDePhoto + "/" + MaPhoto.get_PhotoName()
+                #NouveauPathDePhoto = PhotoPath + "/" + "_A_TRIER_MANUELLEMENT"
+                NouveauPathDePhoto = Path_Photo + "/" + PhotoYear + "/" + PhotoMonth
+                #NouveauNomDePhoto = NouveauPathDePhoto + "/" + MaPhoto.get_PhotoName()
+                NouveauNomDePhoto = NouveauPathDePhoto + "/" + DateConverti + PhotoExtention
 
         print("Photo Move To >>>>>  " + NouveauNomDePhoto)
         if not os.path.exists(NouveauPathDePhoto):
